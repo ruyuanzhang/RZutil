7fae0a2a0fa2beeafeea7606451caef425253451
ruyuanzhang <ruyuanzhang@gmail.com>
2016-11-22 18:24:47 -0600

    add atan2pid function

--- stats/atan2pid.m
+++ stats/atan2pid.m
@@ -1,23 +1,19 @@
 function v=atan2pid(y,x)
 % atan2 function but output the arc range from 0~2*pi
 
-if nargin==1 %just in case the user only gives the value of y myatan(y)
-    x=1;
-end
-v=nan;
 
-if x>0&y>=0
+
+if (~isnan(y))&(~isnan(x))
+if y>0
     v=atan2d(y,x);
 end
-if x>0&y<0
+
+if y<=0
     v=360+atan2d(y,x);
 end
 
-if y>=0 & x<0
-    v=180-atan2d(y,x);
-end
-if y<0 & x<0
-    v=180+atan2d(y,x);
+else
+    v=nan;
 end
 
 end
