ec5c173dc9bb94618c0cd6745506ad5fef0ffeb2
ruyuanzhang <ruyuanzhang@gmail.com>
2017-01-23 14:56:47 -0600

    upgrade mycolororder

--- figures/mycolororder.m
+++ figures/mycolororder.m
@@ -1,11 +1,11 @@
 function c0=mycolororder(theme,n)
 %two different types of color order when making figures
 % Input
-%   theme: color
+%   theme: color,current accept,'color','gray','jet'
 %   n: levels we want, default is 7, we can create more by color
 %       interpolate
 % Output:
-%   c0: a 3 x n matrix rgb value
+%   c0: a n x 3 matrix rgb value
 %   
 % future work: Improve the gray color set
 %
@@ -14,11 +14,11 @@ if (~exist('theme','var')||isempty(theme))
 end
 
 if (~exist('n','var')||isempty(n))
-    n = 0;
+    n = 7;
 end
 
 switch theme
-    case 'color'
+    case 'color' % this is default color order since matlab 2014b
         c0=[0    0.4470    0.7410
             0.8500    0.3250    0.0980
             0.9290    0.6940    0.1250
@@ -26,13 +26,15 @@ switch theme
             0.4660    0.6740    0.1880
             0.3010    0.7450    0.9330
             0.6350    0.0780    0.1840];
+        if n > 7
+            c0=colorinterpolate(c0,ceil(n/7),2);
+        end
+            c0=c0(1:n,:);
     case 'gray'
-        c0=linspace(0,1,7)'*ones(1,3);
+        c0=linspace(0,1,n)'*ones(1,3);
     case 'jet'
+        c0=jet(n);
 end
 
-if n > 7
-    c0=colorinterpolate(c0,n,1);
-end
 
 end
