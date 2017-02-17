523d033d8509dd0b51aa3509d98a61c6d6cee574
ruyuanzhang <ruyuanzhang@gmail.com>
2017-02-16 21:47:59 -0600

    change fitnonlinearmodel_rz

--- modelfitting/fitnonlinearmodel_rz.m
+++ modelfitting/fitnonlinearmodel_rz.m
@@ -1,46 +1,61 @@
-function output = fitnonlinearmodel_rz(data,model,modelinput,init,varargin)
+function output = fitnonlinearmodel_rz(input)
 % function output = fitnonlinearmodel_rz(data,model,modelinput,init,varargin)
 %
 % a simple wrapper fminsearch function for model fitting
-% Input:
+% Input: Input is a structure with fields as below
 %   <positional argument>
-%       data: fitted data, can be
-%       model: constructed model outside of this function. Any function or
-%           model with multiple input argument can be rewritten as a
-%           generally two input, e.g. model(p,input).p is the parameter
-%           array,input is a cell array that include all other argument.
-%           check example below
-%       modelinput: model input mentioned above
-%       init: a vector that set as the initial value for parameters
-%   <parameter argument>, this function can also be provide some other
+%       data: data to fit, typically a time x voxels, response (beta,time series)
+%       model: the model function which only accept a 1xP parameter and a input, @model(p,num);     
+%       modelinput: model input mentioned above, e.g. stimulus
+%       init: a 1xP vector that set as the initial value for parameters
+%   <optional argument>, this function can also be provide some other
 %   parameter argument, write format like, 'bound',[-inf,-inf;inf,inf];
 %       metric:metric function to minimize,could be
 %               @calcleastsquare (default): minimize least square
 %               @calcsse : minimize sse
 %               @calccorrelation : maximize correlation
+%               @calcord: similar to calcsse,proportion of variance, maximize
+%       optimoption: optimization options.
+%       LB: a 1xP vector, low bound during optimization,default:-1e6
+%       UB: a 1xP vector, up bound during optimization, default:1e6
+%       scale: 'linear'(default) or 'log', on which scale we perform
+%           optimization, sometimes we want to optimization in log scale,
+%           e.g. contrast response function
+%       multiinit: a scalar indicating how many random initial values.
+%           Providing a >1 number will overwrite the init. But init is a
+%           NxP matrix, then multinit will be discard. To generate
+%           multiple p0, basically uniformly draw a sample between LB and
+%           UB, in this case, LB and UB should not include [],NaN or
+%           Inf/-Inf
+%       
 %               
 
 % history
 %   1. implement multiple random initial values
 %   2. implement bootstrap and across validation
+%   02/14/2017. update calcord metric, this is a function written by KK
 %   12/24/2016. Ruyuan Zhang 
 % 
 
 
 %% deal with input
 p=myinputParser;
-addRequired(p,'data', @(x) validateattributes(x,{'numeric','cell'},{'nonempty'},1));
-addRequired(p,'model', @(x) validateattributes(x,{'function_handle'},{'nonempty'},2));
-addRequired(p,'modelinput', @(x) validateattributes(x,{'numeric','cell'},{'nonempty'},3));
-addRequired(p,'init',@(x) validateattributes(x,{'numeric'},{'nonempty'},4));
+% some key parameters, 
+addParameter(p,'data',[], @(x) validateattributes(x,{'numeric','cell'},{'nonempty'}));
+addParameter(p,'modelinput',[], @(x) validateattributes(x,{'numeric','cell'},{'nonempty'}));
+addParameter(p,'model',[], @(x) validateattributes(x,{'functional_handle'},{'nonempty'}));
+addParameter(p,'init',[], @(x) validateattributes(x,{'numeric'},{'nonempty'})); % must provide a initial guess
 
-defaultMetric = @calcleastsquare;
-defaultLB = repmat(-inf,1,size(init,2));
-defaultUB = repmat(inf,1,size(init,2));
-defaultOpt = optimset('MaxFunEvals',6000,...
-                        'MaxIter',4000,...
-                        'FunValCheck','on');
+%
+defaultMetric = @calcsse;
+defaultLB = -inf(1,size(input.init,2));
+defaultUB = inf(1,size(input.init,2));
+defaultOpt = optimset(getoptimizeopt('fminsearch'),'MaxFunEvals',1000,...
+                        'MaxIter',500,...
+                        'FunValCheck','on',...
+                        'Display','final');
 defaultScale='linear';
+
 
 addParameter(p,'metric',defaultMetric,@(x) validateattributes(x,{'function_handle'},{'nonempty'}));
 addParameter(p,'opt',defaultOpt,@iscell);
@@ -48,7 +63,7 @@ addParameter(p,'LB',defaultLB, @(x) validateattributes(x,{'numeric'},{'nonempty'
 addParameter(p,'UB',defaultUB, @(x) validateattributes(x,{'numeric'},{'nonempty'}));
 addParameter(p,'scale',defaultScale, @(x) validateattributes(x,{'char'},{'nonempty'}));
 addParameter(p,'multiinit',defaultMetric,@(x) validateattributes(x,{'function_handle'},{'nonempty'}));
-parse(p,data,model,modelinput,init,varargin{:});
+parse(p,input);
 
 %% read in input
 input=p.Results;
@@ -56,10 +71,11 @@ input=p.Results;
 % deal with metric
 metric = input.metric;
 scale  = input.scale;
-if isequal(model,@calcsse)
-    assert(numel(sse),'only fit 1 data point, cannot calculate sse');        
-elseif isequal(model,@calcsse)
-        metric = 1-@calcorrelation; % maxima correlation is equivelant minimize it
+if isequal(metric,@calcsse)
+        assert(numel(data)>1,'only fit 1 data point, cannot calculate sse');        
+elseif ismember(metric,{@calccorrelation,@calccod})
+        metric = @() 1-metric; % maxima correlation is equivelant minimize error
+        % here has some problem.. need to fix;
 end
 
 % deal with model and model input
