358bf4a2c23d304d04314ee572972dc96536d13f
Ruyuan Zhang <ruyuanzhang@gmail.com>
2020-03-11 22:43:08 -0400

    remove rzcellfun

--- programming/rzcellfun.m
+++
@@ -1,5 +0,0 @@
-function result = rzcellfun(varargin)
-% Just a quick wrap for cellfun and so no need to input 'UniformOutput',
-% false repeatly
-
-result = cellfun(varargin{:}, 'UniformOutput', 0);
