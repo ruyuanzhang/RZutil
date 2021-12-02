bd28c6f630f73d6eb655b3b502be17146bab341f
Ru-Yuan Zhang <ruyuanzhang@gmail.com>
2021-12-02 22:00:22 +0800

    20211202

--- string/gettimestr.m
+++ string/gettimestr.m
@@ -5,7 +5,7 @@ function s = gettimestr(t)
 % clock.
 %
 % t is a 1x6 vector indicate time. default s=clock
-if notDefined(t)
+if notDefined('t')
     t = clock;
 end
 s = sprintf('%d%02d%02d%02d%02d%02d',t(1),t(2),t(3),t(4),t(5),round(t(6)));
